f12508a137a65c9b28291f92c2c23e99d5a2e627
KTechy <ktechy19@gmail.com>
2021-08-03 13:54:50 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 yum install unzip -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
 clear
-echo "Katacoda Centos Windows 11 by fb.com/thuong.hai.581"
+echo "Windows 11 RDP Free By KTechy"
 read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
@@ -21,7 +21,7 @@ custom_param_ram="-m "$(expr $availableRAM - 856 )"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 clear
-echo "Katacoda Centos Windows 11 by fb.com/thuong.hai.581"
+echo "Free Windows 11 RDP by KTechy"
 echo Your RDP IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
